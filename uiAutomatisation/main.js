0fba90c137644e762f80bfcdd6f93d706558a012
azertypow <azertypow@gmail.com>
2020-01-31 14:01:31 +0100

    feat: create global process to prevent memory error

--- uiAutomatisation/main.js
+++ uiAutomatisation/main.js
@@ -1,124 +1,18 @@
-const robotJS   = require("robotjs")
 const cp        = require("child_process")
-const fs        = require("fs")
-const { windowManager } = require("node-window-manager")
 
-const illustratorOpeningTime_millisecond = 10000
 
-const defaultMouseDelay = 500
+function startPrinting() {
+    cp.exec("node startPrinting.js", (error, stdout, stderr) => {
+        if(error) {
+            console.error("can't print document", error)
+        } else {
 
-const posePad = {
-    x: 0,
-    y: 60,
-}
+            console.log(stdout)
+            console.log(stderr)
 
-const mousePosition = {
-    illustrator: {
-        fichier: {
-            x: 160  - posePad.x,
-            y: 72   - posePad.y,
-        },
-        fichier__imprimer: {
-            x: 250 - posePad.x,
-            y: 570 - posePad.y,
-        },
-        printerWindow: {
-            x: 922 - posePad.x,
-            y: 853 - posePad.y,
+            setTimeout(startPrinting, 10 * 60_000)
         }
-    }
-}
-
-const ILLUSTRATOR_APPLICATION_PATH = "/Applications/Adobe\\ Illustrator\\ 2020/Adobe\\ Illustrator.app"
-
-let imageCaptureCounter = 0
-
-let printedPapper = 1
-
-function printADocument() {
-
-    robotJS.setMouseDelay(defaultMouseDelay)
-
-    cp.exec(`open ${ILLUSTRATOR_APPLICATION_PATH}`, (error, stdout, stderr) => {
-
-
-        setTimeout(() => {
-            windowManager.requestAccessibility()
-
-            const window = windowManager.getActiveWindow()
-
-            // Prints the currently focused window title
-            console.log("focused window instance: ", window)
-            console.log("focused window title: ", window.getTitle())
-
-            robotJS.setMouseDelay(illustratorOpeningTime_millisecond)
-
-            robotJS.moveMouseSmooth(mousePosition.illustrator.fichier.x, mousePosition.illustrator.fichier.y)
-
-            robotJS.setMouseDelay(defaultMouseDelay)
-
-            robotJS.mouseClick()
-
-            robotJS.moveMouseSmooth(mousePosition.illustrator.fichier__imprimer.x, mousePosition.illustrator.fichier__imprimer.y)
-
-            robotJS.mouseClick()
-
-            const image = robotJS.screen.capture()
-
-            console.info("screenSize: ", image.width, " x ", image.height)
-
-            // fs.writeFileSync(`screenCapture-${imageCaptureCounter}.raw`, image.image)
-
-            imageCaptureCounter++
-
-            robotJS.moveMouseSmooth(mousePosition.illustrator.printerWindow.x, mousePosition.illustrator.printerWindow.y)
-
-            robotJS.mouseClick()
-
-            console.log("printed paper: ", printedPapper)
-            printedPapper++
-
-            setTimeout(printADocument, 15 * 60_000)
-
-        }, 1000)
-
     })
 }
-printADocument()
 
-// ===== test 1
-
-// windowManager.requestAccessibility();
-//
-// const window = windowManager.getActiveWindow();
-//
-// // Prints the currently focused window title.
-// console.log(window.getTitle());
-
-// =====
-
-// ===== test 2
-
-// const test = require("node-window-manager")
-//
-// console.log(test)
-//
-// /**@type {Array<Number>}*/
-// const listArrayOfWindowID = test.addon.getWindows()
-//
-// console.log(listArrayOfWindowID)
-//
-// for(const windowID of listArrayOfWindowID) {
-//     // console.log(test.addon.getWindowTitle(windowID))
-//
-//     const newWindow = new test.Window(windowID)
-//
-//     console.log(newWindow)
-// }
-//
-// const window = windowManager.getActiveWindow()
-//
-// // Prints the currently focused window bounds.
-// console.log(window.getBounds())
-//
-// console.log(window)
+startPrinting()
---
+++ uiAutomatisation/startPrinting.js
@@ -0,0 +1,124 @@
+const robotJS   = require("robotjs")
+const cp        = require("child_process")
+const fs        = require("fs")
+const { windowManager } = require("node-window-manager")
+
+const illustratorOpeningTime_millisecond = 10000
+
+const defaultMouseDelay = 500
+
+const posePad = {
+    x: 0,
+    y: 60,
+}
+
+const mousePosition = {
+    illustrator: {
+        fichier: {
+            x: 160  - posePad.x,
+            y: 72   - posePad.y,
+        },
+        fichier__imprimer: {
+            x: 250 - posePad.x,
+            y: 570 - posePad.y,
+        },
+        printerWindow: {
+            x: 922 - posePad.x,
+            y: 853 - posePad.y,
+        }
+    }
+}
+
+const ILLUSTRATOR_APPLICATION_PATH = "/Applications/Adobe\\ Illustrator\\ 2020/Adobe\\ Illustrator.app"
+
+let imageCaptureCounter = 0
+
+let printedPapper = 1
+
+function printADocument() {
+
+    robotJS.setMouseDelay(defaultMouseDelay)
+
+    cp.exec(`open ${ILLUSTRATOR_APPLICATION_PATH}`, (error, stdout, stderr) => {
+
+
+        setTimeout(() => {
+            windowManager.requestAccessibility()
+
+            const window = windowManager.getActiveWindow()
+
+            // Prints the currently focused window title
+            console.log("focused window instance: ", window)
+            console.log("focused window title: ", window.getTitle())
+
+            robotJS.setMouseDelay(illustratorOpeningTime_millisecond)
+
+            robotJS.moveMouseSmooth(mousePosition.illustrator.fichier.x, mousePosition.illustrator.fichier.y)
+
+            robotJS.setMouseDelay(defaultMouseDelay)
+
+            robotJS.mouseClick()
+
+            robotJS.moveMouseSmooth(mousePosition.illustrator.fichier__imprimer.x, mousePosition.illustrator.fichier__imprimer.y)
+
+            robotJS.mouseClick()
+
+            const image = robotJS.screen.capture()
+
+            console.info("screenSize: ", image.width, " x ", image.height)
+
+            // fs.writeFile(`screenCapture-${imageCaptureCounter}.raw`, image.image, {encoding: "base64"}, err => {
+            //     if( err ) {
+            //         console.log(err)
+            //     }
+            // })
+            // imageCaptureCounter++
+
+            robotJS.moveMouseSmooth(mousePosition.illustrator.printerWindow.x, mousePosition.illustrator.printerWindow.y)
+
+            robotJS.mouseClick()
+
+            console.log("printed paper: ", printedPapper)
+            printedPapper++
+        }, 1000)
+
+    })
+}
+printADocument()
+
+// ===== test 1
+
+// windowManager.requestAccessibility();
+//
+// const window = windowManager.getActiveWindow();
+//
+// // Prints the currently focused window title.
+// console.log(window.getTitle());
+
+// =====
+
+// ===== test 2
+
+// const test = require("node-window-manager")
+//
+// console.log(test)
+//
+// /**@type {Array<Number>}*/
+// const listArrayOfWindowID = test.addon.getWindows()
+//
+// console.log(listArrayOfWindowID)
+//
+// for(const windowID of listArrayOfWindowID) {
+//     // console.log(test.addon.getWindowTitle(windowID))
+//
+//     const newWindow = new test.Window(windowID)
+//
+//     console.log(newWindow)
+// }
+//
+// const window = windowManager.getActiveWindow()
+//
+// // Prints the currently focused window bounds.
+// console.log(window.getBounds())
+//
+// console.log(window)
