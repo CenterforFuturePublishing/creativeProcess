c9d001581eb954840b1f15af1a517be878f15ee8
azertypow <azertypow@gmail.com>
2020-02-10 14:11:33 +0100

    feat: init layout

--- PdfGenerator/src/main.js
+++ PdfGenerator/src/main.js
@@ -1,11 +1,35 @@
 const puppeteer = require('puppeteer');
 
-puppeteer.launch().then(async browser => {
+const css = `
+            body {
+                margin: 0;
+                color: red;
+            }
+            
+            .container {
+                background: azure;
+                width: 100%;
+            }
+        `
+
+puppeteer.launch({headless: false}).then(async browser => {
     const page = await browser.newPage();
 
     const result = await page.evaluate(data => {
-        document.body.style.width = '1000px'
 
+        // style
+        const styleContainer = document.createElement("style")
+        styleContainer.innerText = data.style
+        document.head.appendChild(styleContainer)
+
+        // column
+        const container = document.createElement("div")
+        container.classList.add(".container")
+
+        document.body.appendChild(container)
+
+
+        // text Elemeent
         const textElement = document.createElement("div")
         textElement.innerText = data.text
 
@@ -14,6 +38,7 @@ puppeteer.launch().then(async browser => {
         return document.body.getBoundingClientRect().width;
     }, {
         text: "coucou david",
+        style: css
     });
 
     await page.pdf({
