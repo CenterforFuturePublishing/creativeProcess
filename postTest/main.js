3c2b43702e58dae5adf867ffe4f56e80b45b57a5
azertypow <azertypow@gmail.com>
2020-02-24 11:22:07 +0100

    feat: update test

--- postTest/main.js
+++ postTest/main.js
@@ -1,14 +1,17 @@
 const axios = require('axios')
 
 axios.post('http://localhost:3000/api/poem', {
-    poem: "test très simplé\ncoucou comment allez vous?",
-    graisse: 5000,
-    contraste: 50,
-    rigidite: 25,
+    poem: "Des petits et des des. Le passant s'est mise.\n" +
+        "Cette soleil de l'autre souffle d'une devant,\n" +
+        'Où la monte se repose et se retire en son voyage\n' +
+        'Le muse est le reste ; le plus sonore et sa mai !',
+    graisse: 30,
+    contraste: 0,
+    rigidite: 0
 })
     .then((res) => {
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(res)
+        // console.log(`statusCode: ${res.statusCode}`)
+        console.log(res.data)
     })
     .catch((error) => {
         console.error(error)
