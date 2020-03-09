246b083fb2379be8d973a4deaf9208f02dd75a35
azertypow <azertypow@gmail.com>
2020-03-09 14:10:42 +0100

    feat: test wit array correction

--- postTest/main.js
+++ postTest/main.js
@@ -1,18 +1,56 @@
 const axios = require('axios')
 
-axios.post('http://localhost:3000/api/poem', {
-    poem: "Des petits et des des. Le passant s'est mise.\n" +
-        "Cette soleil de l'autre souffle d'une devant,\n" +
-        'Où la monte se repose et se retire en son voyage\n' +
-        'Le muse est le reste ; le plus sonore et sa mai !',
-    graisse: 30,
-    contraste: 0,
-    rigidite: 0
-})
-    .then((res) => {
-        // console.log(`statusCode: ${res.statusCode}`)
-        console.log(res.data)
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+postArrayOfPoemData([
+    {
+        poem: "Des petits et des des. Le passant s'est mise.\n" +
+            "Cette soleil de l'autre souffle d'une devant,\n" +
+            'Où la monte se repose et se retire en son voyage\n' +
+            'Le muse est le reste ; le plus sonore et sa mai !',
+        graisse: 30,
+        contraste: 0,
+        rigidite: 0,
+    },
+    {
+        poem: "Des petits et des des. Le passant s'est mise.\n" +
+            "Cette soleil de l'autre souffle d'une devant,\n" +
+            'Où la monte se repose et se retire en son voyage\n' +
+            'Le muse est le reste ; le plus sonore et sa mai !',
+        graisse: 30,
+        contraste: 50,
+        rigidite: 80,
+    },
+])
+
+
+function postArrayOfPoemData(arrayOfPoemData, index = 0) {
+
+    const poemData = arrayOfPoemData[index]
+
+    postPoemData(poemData)
+
+    index++
+
+    if (index < arrayOfPoemData.length) {
+        setTimeout(() => {
+
+            postArrayOfPoemData(arrayOfPoemData, index)
+
+        }, 1_000)
+    }
+
+}
+
+function postPoemData(poemData) {
+
+    console.log("post poem:", poemData)
+
+    axios.post('http://localhost:3000/api/poem', poemData)
+        .then((res) => {
+            // console.log(`statusCode: ${res.statusCode}`)
+            console.log(res.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+}
